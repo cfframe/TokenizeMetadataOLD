05f70199399bad91de300ca6f8fb6a7281c81599
ColinFF <3910544+cfframe@users.noreply.github.com>
2021-11-26 14:01:14 +0000

    Handle directory of files. Add test files (missed out earlier).

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 # main.py
 import argparse
-import csv
 import datetime
 from src.file_tools import FileTools
 from src.meta_data_tools import MetaDataTools
@@ -13,14 +12,17 @@ TEST_FILEPATH = os.path.join(Path(__file__).parent, 'tests', 'test_data', 'test_
 Description: Read in a data dictionary file and output a tokenized version.
 
 Example usage:
-py main.py -sf "C:\temp\TableMetaData\Source\SAP IS-H Case Attribute.txt" -wd C:\temp\TableMetaData\Results 
+py main.py -sf "C:/temp/TableMetaData/Source/SAP IS-H Case Attribute.txt" -wd C:/temp/TableMetaData/Results 
+py main.py -sf "C:/temp/TableMetaData/Source" -d -wd C:/temp/TableMetaData/Results 
 """
 
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Extract fields and tokenized descriptors from data dictionary.')
-    parser.add_argument('-sf', '--src_filepath', type=str, default=TEST_FILEPATH,
-                        help="Source file for processing")
+    parser.add_argument('-sf', '--src_path', type=str, default=TEST_FILEPATH,
+                        help='Source path for processing. Assume a file, but use is_directory flag if a folder.')
+    parser.add_argument('-d', '--is_directory', action='store_true',
+                        help='Indicates src_path is a directory and not a file')
     parser.add_argument('-wd', '--working_dir', type=str, default=Path(__file__).parent,
                         help='Working directory for downloads etc (optional)')
 
@@ -29,30 +31,42 @@ def parse_args():
     return args
 
 
-def main():
-    args = parse_args()
-    # Declare args - helps with auto-completion. Convert to object?
-    args.src_filepath = args.src_filepath
-    args.working_dir = args.working_dir
-
-    runtime_string = datetime.datetime.now().strftime('%y%m%d_%H%M%S_')
-    command_filename = f'{runtime_string}Command args.txt'
-    FileTools.save_command_args_to_file(vars(args),
-                                        save_path=os.path.join(Path(args.working_dir).parent, command_filename),
-                                        to_print=True
-                                        )
-
-    df = MetaDataTools.read_raw_data(args.src_filepath)
+def process_file(src_path: str, working_dir: str, runtime_string: str):
+    df = MetaDataTools.read_raw_data(src_path)
 
     field_descriptors = MetaDataTools.field_tokenized_descriptor_df_from_df(df)
-    save_name = f'{runtime_string}{Path(args.src_filepath).stem}_ProcessedDF.txt'
-    save_path = os.path.join(Path(args.working_dir), save_name)
+    save_name = f'{runtime_string}_ProcessedDF {Path(src_path).stem}.txt'
+    save_path = os.path.join(Path(working_dir), save_name)
     # Open file with newline='' to prevent blank intermediate lines
     with open(save_path, 'w', encoding='utf-8', newline='') as outfile:
         outfile.write(field_descriptors.to_csv(sep='\t'))
-        print('2nd Tokenized file saved to {}.'.format(save_path))
+        print('Tokenized file saved to {}.'.format(save_path))
+
+    return
+
+
+def main():
+    args = parse_args()
+    # Declare args - helps with auto-completion. Convert to object?
+    src_path = args.src_path
+    working_dir = args.working_dir
+    is_directory = args.is_directory
+
+    runtime_string = datetime.datetime.now().strftime('%y%m%d_%H%M%S')
+    command_filename = f'{runtime_string} Command args.txt'
+    FileTools.save_command_args_to_file(vars(args),
+                                        save_path=os.path.join(Path(working_dir).parent, command_filename),
+                                        to_print=True
+                                        )
+
+    if not is_directory:
+        process_file(src_path, working_dir, runtime_string)
+    else:
+        for root, dirs, files in os.walk(src_path, topdown=False):
+            for file in files:
+                file_path = os.path.join(src_path, file)
+                process_file(file_path, working_dir, runtime_string)
 
 
 if __name__ == '__main__':
     main()
-
