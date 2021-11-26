c774d31ee9ab4916a764ee3155a31e82d2aa8afc
ColinFF <3910544+cfframe@users.noreply.github.com>
2021-11-26 09:06:50 +0000

    Update method descriptions. Add field_tokenized_descriptor_list_from_df. Sub tests - swap out testing_for for a print statement.

--- src/meta_data_tools.py
+++ src/meta_data_tools.py
@@ -14,7 +14,7 @@ class MetaDataTools:
         """
         Cleanse textual data held in a DataFrame. Excludes column names.
 
-        :param df: DataFrame to process
+        :param df: DataFrame to process.
         :returns: DataFrame with text cleansed.
         """        
         new_df = pd.DataFrame()
@@ -30,12 +30,13 @@ class MetaDataTools:
 
     @staticmethod
     def cleanse_text(text: str) -> list:
-        """Pre process text prior to tokenizing
-        Make lower case
-        Replace apostrophes and quotes with nothing, other punctuation with a space
+        """Pre process text prior to tokenizing.
 
-        :param text: original text
-        :return: processed text as list
+        Make lower case.
+        Remove apostrophes and quotes, replace other punctuation with a space.
+
+        :param text: Original text.
+        :return: Processed text as list.
         """
         # Make all lower case first
         text = str.lower(text)
@@ -57,10 +58,11 @@ class MetaDataTools:
 
     @staticmethod
     def stemming(tokenized_text: list, stemmer=nltk.LancasterStemmer()):
-        """Stem stemmed_text by optionally chosen stemmer
-        :param tokenized_text: list of words to be stemmed; assumes already pre-processed
+        """Stem stemmed_text by optionally chosen stemmer.
+
+        :param tokenized_text: List of words to be stemmed; assumes already pre-processed.
         :param stemmer: NLTK stemmer (default LancasterStemmer)
-        :return: list of stemmed words
+        :return: List of stemmed words.
         """
         stemmed_text = [stemmer.stem(word) for word in tokenized_text]
         return stemmed_text
@@ -71,7 +73,7 @@ class MetaDataTools:
 
         Returns the first column found that may hold a description, going to column name only.
 
-        :param df: DataFrame to process
+        :param df: DataFrame to process.
         :return: List [descriptor column index, original descriptor column name]. If none found, returns [-1, ''].
         """
         lancaster = nltk.LancasterStemmer()
@@ -89,8 +91,33 @@ class MetaDataTools:
         for column in stemmed_columns:
             if descriptor_stem in column:
                 descriptor_column_index = stemmed_columns.index(column)
+                descriptor_column = df.columns[descriptor_column_index]
+
                 break
 
-        descriptor_column = df.columns[descriptor_column_index]
-
         return [descriptor_column_index, descriptor_column]
+
+    @staticmethod
+    def field_tokenized_descriptor_list_from_df(df: pd.DataFrame) -> list:
+        """Derive a list of field names against descriptions from a DataFrame.
+
+        Assume that Field names are the first column, and that if only two columns then
+        the second column is the descriptors.
+
+        :param df: Source DataFrame.
+        :returns: See description.
+        """
+
+        if len(df.columns) < 2:
+            raise Exception('Data set has too few columns.')
+        elif len(df.columns) == 2:
+            descriptor_column_index = 1
+        else:
+            descriptor_column_index = MetaDataTools.identify_descriptor_column(df)[0]
+            if descriptor_column_index < 0:
+                raise Exception('No descriptor column identified for DataFrame.')
+
+        field_names = df[df.columns[0]]
+        descriptions = [MetaDataTools.cleanse_text(text) for text in df[df.columns[descriptor_column_index]]]
+
+        return [field_names, descriptions]
--- tests/meta_data_tools_test_case.py
+++ tests/meta_data_tools_test_case.py
@@ -13,20 +13,20 @@ class MetaDataToolsTestCase(unittest.TestCase):
         """Fixtures used by tests."""
         self.Root = Path(__file__).parent
         self.TestDataDir = os.path.join(self.Root, 'test_data')
-        self.TsvFilePath = os.path.join(self.TestDataDir, 'test_tsv.txt')
-        self.TestDataFrame = MDT.read_raw_data(self.TsvFilePath)
-        self.NoDescTsvFilePath = os.path.join(self.TestDataDir, 'test_no_desc_tsv.txt')
-        self.TestNoDescDataFrame = MDT.read_raw_data(self.NoDescTsvFilePath)
+        self.Test4ColDataFrame = MDT.read_raw_data(os.path.join(self.TestDataDir, 'test_tsv_4_cols.txt'))
+        self.TestNoDescDataFrame = MDT.read_raw_data(os.path.join(self.TestDataDir, 'test_no_desc_tsv.txt'))
+        self.Test1ColDataFrame = MDT.read_raw_data(os.path.join(self.TestDataDir, 'test_tsv_1_col.txt'))
+        self.Test2ColDataFrame = MDT.read_raw_data(os.path.join(self.TestDataDir, 'test_tsv_2_cols.txt'))
 
     def test_read_raw_data__raw_data_has_expected_shape(self):
-        df = MDT.read_raw_data(self.TsvFilePath)
+        df = self.Test4ColDataFrame
 
         pd.set_option('display.max_colwidth', 100)
         print(df.head())
         self.assertEqual(df.shape, (2, 4))
 
     def test_cleanse_text_in_dataframe__returns_dataframe_without_punctuation(self):
-        test_df = self.TestDataFrame
+        test_df = self.Test4ColDataFrame
         new_df = MDT.cleanse_text_in_dataframe(test_df)
 
         expected = ['institution']
@@ -35,12 +35,14 @@ class MetaDataToolsTestCase(unittest.TestCase):
 
     def test_cleanse_text__remove_specified_punctuation(self):
         expected = 'dogs kennels paint'
-        with self.subTest(self, testing_for='Apostrophe'):
+        with self.subTest(self):
+            print(f'Testing for: Apostrophe')
             test_text = "Dog's kennel's paint"
             actual = ' '.join(MDT.cleanse_text(test_text))
             self.assertEqual(expected, actual)
 
-        with self.subTest(self, testing_for='Double quote'):
+        with self.subTest(self):
+            print(f'Testing for: Double quote')
             test_text = 'Dogs "kennels" paint'
             actual = ' '.join(MDT.cleanse_text(test_text))
             self.assertEqual(expected, actual)
@@ -52,7 +54,8 @@ class MetaDataToolsTestCase(unittest.TestCase):
                      ['Adjacent punctuation', "Dog's kennel=dog****house&&&"]]
 
         for sub_test in sub_tests:
-            with self.subTest(self, testing_for=sub_test[0]):
+            with self.subTest(self):
+                print(f'Testing for: {sub_test[0]}')
                 test_text = sub_test[1]
                 actual = MDT.cleanse_text(test_text)
                 self.assertEqual(expected, actual)
@@ -62,27 +65,53 @@ class MetaDataToolsTestCase(unittest.TestCase):
         sub_tests = [['Porter', nltk.PorterStemmer(), ['describ', 'describ', 'descriptor', 'descript']],
                      ['Lancaster', nltk.LancasterStemmer(), ['describ', 'describ', 'describ', 'describ']]]
         for sub_test in sub_tests:
-            with self.subTest(self, testing_for=sub_test[0]):
+            with self.subTest(self):
+                print(f'Testing for: {sub_test[0]}')
                 stemmer = sub_test[1]
                 expected = sub_test[2]
                 actual = MDT.stemming(test_words, stemmer)
                 self.assertEqual(expected, actual)
 
     def test_identify_descriptor_column__when_valid_column_name_exists__returns_index_and_name(self):
-        df = self.TestDataFrame
+        df = self.Test4ColDataFrame
         expected = 1
         # Column index is first part of pairing returned
-        actual = MDT.identify_descriptor_column(self.TestDataFrame)[0]
+        actual = MDT.identify_descriptor_column(self.Test4ColDataFrame)[0]
 
         self.assertEqual(expected, actual)
 
     def test_identify_descriptor_column__when_valid_column_name_not_exists__returns_dummy_index_and_name(self):
-        df = self.TestNoDescDataFrame
-
         expected = -1
         # Column index is first part of pairing returned
         actual = MDT.identify_descriptor_column(self.TestNoDescDataFrame)[0]
 
+        self.assertEqual(expected, actual)
+
+    def test_field_tokenized_descriptor_list_from_df__when_valid__returns_paired_series(self):
+        # Column index is first part of pairing returned
+        sub_tests = [['2 columns', self.Test2ColDataFrame], ['4 columns', self.Test4ColDataFrame]]
+        for sub_test in sub_tests:
+            with self.subTest(self):
+                print(f'Testing for: {sub_test[0]}')
+                fdl = MDT.field_tokenized_descriptor_list_from_df(sub_test[1])
+
+                # ALSO TEST for when no descriptor identified - decide whether to proceed or not
+                self.assertTrue(len(fdl) == 2)
+                self.assertTrue(fdl[0].__class__.__name__ == 'Series')
+                self.assertTrue(fdl[1].__class__.__name__ == 'Series')
+
+    def test_field_tokenized_descriptor_list_from_df__when_invalid__raises_exception(self):
+        # Column index is first part of pairing returned
+        sub_tests = [['1 column', self.Test1ColDataFrame], ['No descriptor', self.TestNoDescDataFrame]]
+        for sub_test in sub_tests:
+            with self.subTest(self):
+                print(f'Testing for: {sub_test[0]}')
+                self.assertRaises(Exception, MDT.field_tokenized_descriptor_list_from_df, sub_test[1])
+
+    def test_field_tokenized_descriptor_list_from_df__when_valid__tokenizes_data(self):
+        fdl = MDT.field_tokenized_descriptor_list_from_df(self.Test2ColDataFrame)
+        expected = ['institution']
+        actual = fdl[1][0]
         self.assertEqual(expected, actual)
 
 
